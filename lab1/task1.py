48b05c6f8fc3ee1acc64e344a6a2e9a756d0ac4d
Gn0mbR-Molb <mkondratuk373@gmail.com>
2026-09-22 20:26:23 +1000

    feat: start lab 1

---
+++ lab1/task1.py
@@ -0,0 +1,9 @@
+station_name = "Ocean"
+active_sensors = 18
+average_temperature = 21.6
+is_online = True
+print(station_name, type("Ocean"))
+print(active_sensors, type(18))
+print(average_temperature, type(21.6))
+print(is_online, type(True))
+
